71f866d569b4b37a1eefd1df11f7e1700d532fee
spencydot <spencerjtrott@gmail.com>
2022-03-29 15:25:02 +0100

    Create aciiArt.py

---
+++ aciiArt.py
@@ -0,0 +1,62 @@
+banner = """
+$$$$$$$$\           $$$$$$\  $$$$$$$$\        $$$$$$\            $$\                           $$$$$$\                                          $$\   $$\                     
+\____$$  |         $$ ___$$\ \____$$  |      $$  __$$\           $$ |                         $$  __$$\                                         \__|  $$ |                    
+    $$  /$$$$$$$\  \_/   $$ |    $$  /       $$ /  \__|$$\   $$\ $$$$$$$\   $$$$$$\   $$$$$$\ $$ /  \__| $$$$$$\   $$$$$$$\ $$\   $$\  $$$$$$\  $$\ $$$$$$\   $$\   $$\       
+   $$  / $$  __$$\   $$$$$ /    $$  /        $$ |      $$ |  $$ |$$  __$$\ $$  __$$\ $$  __$$\\$$$$$$\  $$  __$$\ $$  _____|$$ |  $$ |$$  __$$\ $$ |\_$$  _|  $$ |  $$ |      
+  $$  /  $$ |  $$ |  \___$$\   $$  /         $$ |      $$ |  $$ |$$ |  $$ |$$$$$$$$ |$$ |  \__|\____$$\ $$$$$$$$ |$$ /      $$ |  $$ |$$ |  \__|$$ |  $$ |    $$ |  $$ |      
+ $$  /   $$ |  $$ |$$\   $$ | $$  /          $$ |  $$\ $$ |  $$ |$$ |  $$ |$$   ____|$$ |     $$\   $$ |$$   ____|$$ |      $$ |  $$ |$$ |      $$ |  $$ |$$\ $$ |  $$ |      
+$$  /    $$ |  $$ |\$$$$$$  |$$  /           \$$$$$$  |\$$$$$$$ |$$$$$$$  |\$$$$$$$\ $$ |     \$$$$$$  |\$$$$$$$\ \$$$$$$$\ \$$$$$$  |$$ |      $$ |  \$$$$  |\$$$$$$$ |      
+\__/     \__|  \__| \______/ \__/             \______/  \____$$ |\_______/  \_______|\__|      \______/  \_______| \_______| \______/ \__|      \__|   \____/  \____$$ |      
+                                                       $$\   $$ |                                                                                             $$\   $$ |      
+                                                       \$$$$$$  |                                                                                             \$$$$$$  |      
+                                                        \______/                                                                                               \______/       
+         .,oo8888888888888888ooo,.                                                         
+    ,od88888888888888888888888888oo,                                                                                                
+ ,o0MMMMMMMMNMMMMM8888888888888888888o.                                   Updates:                                                                       
+d888888888V'.o   ```VoooooooOOOOOOOOIII,                                  Added New discount code to get 33% of of your next purchase!+ New Subscription                  
+l888LLLLl:  O , ,O    ``VlMM88888IIAMMMMMOMb,                             package for returning coustmers!                                          
+l8888888LLb `VooV',O,MoodlM88888IIIMMMMV'ddMOMb,                          Discount Code: "JDNU-28HF-2021"                                                  
+l88888888888booooooOlllllIIIIIIIIIAMMV',dMMOOMMMb,                        Or Use This link: "www.7n37.com/products/discount=JDNU-28HF-2021/
+A888888888888888888LLLLIAMMMMMMMMMMMMMMMMMOOOMMMMMMb,                     
+S8M8888888888LLLMMMAAMMMAAMMMMMMMMMMMMMMOOOOMMMMMMMMb                     In other news we have partnerd woth microsoft help with resent data breached 
+S88M8888888lll888888mmmmmmmmmmmmmmvvvvvvvvvvvvv,.MMMM,                    In large companys, installing and updating DDOS protecton systems. 
+D8888M888888llllllllllllllV::::V''~~        ~~'V lMMV                      
+M8888MMM888888TTTMl8lllllb:::V'                `IiM'                      Our staff have been worling exsta hard and we whould like to thank everyone in these  
+MMMM8888M8k88888l8Mklllllk:A'                  `V'                        trying times <3 - 7n37 Socal-Media Team
+l8MM8MMMM8888888888888MMMMllM                                             
+I8MM8MMMM88888888888MTHHMllA  
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
